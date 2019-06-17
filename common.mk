27130cfc909c608ee43005e5cde1a5a125848250
mrhainavn <mr.hai.na.vn@gmail.com>
2019-06-17 13:20:37 +0700

    Add power off charging animation
    Commit from : https://github.com/covahOS/device_enchilada/commit/795a4f1

--- common.mk
+++ common.mk
@@ -119,6 +119,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     power.qcom:64
 
+# Offmode charging
+ PRODUCT_PACKAGES += \
+     chargeonlymode
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
